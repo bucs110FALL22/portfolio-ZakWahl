69c5ced2838e8cd15057d905062ba1f5190567cf
ZakWahl <112440141+ZakWahl@users.noreply.github.com>
2022-09-02 00:44:00 +0000

    What did I change?

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -1,1 +1,12 @@
-print("10"*"5")
+print(10*5)
+print(10**2)
+print(15/10)
+print(15//10) 
+print(-15//10)
+print(15%10)
+print(10%15)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15) #this last answer goes to infinity and does not stop at 15 decimal points, the last number should be rounded up
