bf8d87f0c621702dcef994b0c547e5b415a0d78c
ZakWahl <112440141+ZakWahl@users.noreply.github.com>
2022-09-06 21:44:12 +0000

    Simple operations and Euro to USD money exchange operation

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -10,3 +10,8 @@ print(15 % 10)
 print(10 % 10)
 print(0 % 10)
 print(10 / 15) #this last answer goes to infinity and does not stop at 15 decimal points, the last number should be rounded up
+rate = float(input("What is the exchange rate for Euro to Dollar?"))
+amount = float(input("What is the amount of currency to exchange?"))
+total= rate * amount
+result= total - 3
+print(result) 
