a531e23fa1a885107327f869adfb799543ee5083
ZakWahl <112440141+ZakWahl@users.noreply.github.com>
2022-10-18 03:59:21 +0000

    Boolean Function Excercise

---
+++ ch05/exercises/boolean_functions.py
@@ -0,0 +1,24 @@
+
+grade = int(input("What is your current grade? "))
+
+def percentage_to_letter(score=0):
+  if score>= 90:
+    print("A")
+  if score>= 80 and score < 90:
+    print("B")
+  if score>= 70 and score <80: 
+    print("D")
+  if score<=59:
+    print("F")
+
+def is_passing(letter=None):
+  if letter in("A", "B", "C"):
+    return True 
+  else:
+    return False 
+
+grade = int(input("What is your current grade? "))
+percentage_to_letter(grade) 
+result = (percentage_to_letter)
+is_passing(result) 
+  
